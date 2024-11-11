3e4601f1add48eca5f129798a73481151dcd0e42
Tim Heuett <tim.heuett@gmail.com>
2024-11-10 19:38:51 -0800

    recreate old site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,48 @@
-import Image from "next/image";
+"use client";
+
+import React, { ReactNode, useEffect } from "react";
+import { Mail, Instagram, Linkedin, GitHub } from "react-feather";
+
+const socials = [
+  { href: "//github.com/timhugh", icon: <GitHub size={16}/> },
+  { href: "//linkedin.com/in/timheuett", icon: <Linkedin size={16}/> },
+  { href: "//instagram.com/timhugh", icon: <Instagram size={16}/> },
+  { href: "mailto:info@timheuett.com", icon: <Mail size={16}/> },
+];
+
+const TextLink = ({href, children}: { href: string, children: ReactNode}) => (
+  <a href={href} className="border-b border-b-primary pb-0.5 hover:text-primary">
+    {children}
+  </a>
+)
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [opacity, setOpacity] = React.useState("opacity-0");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    setOpacity("opacity-100");
+  }, []);
+
+  return (
+    <div className={`flex flex-col md:flex-row justify-center items-center px-10 gap-16 h-screen ${opacity} transition-opacity duration-1000`}>
+      <div className="flex flex-col items-center md:items-end">
+        <h1 className="text-4xl font-thin uppercase mb-6 md:mb-4 text-center">Tim Heuett</h1>
+        <ul className="list-none flex flex-row items-center gap-2 md:mr-1">
+          {
+            socials.map(({ href, icon }, index) => (
+              <li key={index} className="text-background bg-foreground hover:bg-primary p-1.5 rounded-full">
+                <a href={href}>
+                  {icon}
+                </a>
+              </li>))
+          }
+        </ul>
+      </div>
+      <div className="font-light text-md md:text-lg leading-6 text-center md:text-left">
+        <p className="pb-2">Hi, I&apos;m Tim.</p>
+        <p className="pb-2">I live in Seattle, WA.</p>
+        <p>I&apos;m a <TextLink href="//github.com/timhugh">web developer</TextLink> and a <TextLink href="//instagram.com/timhugh">photographer</TextLink>.</p>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "#fff",
+                foreground: "#666",
+                primary: "#84c66f",
+            },
+            fontFamily: {
+                sans: ["Lato", "sans-serif"],
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
