93d7e93aaabd5a7d48c424aa4de554ac20d37951
Tim Heuett <tim.heuett@gmail.com>
2025-03-17 13:39:02 -0700

    make nav links easy to configure

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -5,7 +5,23 @@ import { Mail, Instagram, Linkedin, GitHub } from "react-feather";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const socials = [
+interface SocialLink {
+	href: string;
+	icon: React.ReactNode;
+}
+
+interface NavLink {
+	href: string;
+	label: string;
+}
+
+const navLinks: NavLink[] = [
+	{ href: "/", label: "About" },
+	// { href: "/blog", label: "Blog" },
+	// { href: "/photos", label: "Photos" },
+];
+
+const socials: SocialLink[] = [
 	{ href: "//github.com/timhugh", icon: <GitHub size={16} /> },
 	{ href: "//linkedin.com/in/timheuett", icon: <Linkedin size={16} /> },
 	{ href: "//instagram.com/timhugh", icon: <Instagram size={16} /> },
@@ -13,7 +29,7 @@ const socials = [
 ];
 
 export default function Header() {
-	const pathname = usePathname();
+	const currentPath = usePathname();
 
 	return (
 		<header className="border-b pt-10 pb-10">
@@ -24,33 +40,7 @@ export default function Header() {
 							Tim Heuett
 						</Link>
 					</h1>
-					<nav className="text-2xl">
-						<ul className="list-none flex flex-col lg:flex-row items-center gap-6 lg:gap-14 font-serif">
-							<li>
-								{pathname === "/" ? (
-									<span className="text-gray-500">About</span>
-								) : (
-									<Link href="/">About</Link>
-								)}
-							</li>
-							<li>
-								{pathname === "/blog" ? (
-									<span className="text-gray-500">Blog</span>
-								) : (
-									<Link href="/blog">Blog</Link>
-								)}
-							</li>
-							<li>
-								{pathname === "/photos" ? (
-									<span className="text-gray-500">
-										Photos
-									</span>
-								) : (
-									<Link href="/photos">Photos</Link>
-								)}
-							</li>
-						</ul>
-					</nav>
+					{buildNavLinks(currentPath, navLinks)}
 				</div>
 				<ul className="list-none flex flex-row items-center gap-2 md:mr-1">
 					{socials.map(({ href, icon }, index) => (
@@ -66,5 +56,36 @@ export default function Header() {
 				</ul>
 			</div>
 		</header>
+	);
+}
+
+function buildNavLinks(
+	currentPath: string,
+	navLinks: NavLink[],
+): React.ReactNode {
+	if (navLinks.length <= 1) {
+		return null;
+	}
+	return (
+		<nav className="text-2xl">
+			<ul className="list-none flex flex-col lg:flex-row items-center gap-6 lg:gap-14 font-serif">
+				{navLinks.map((link) => buildNavLink(currentPath, link))}
+			</ul>
+		</nav>
+	);
+}
+
+function buildNavLink(
+	currentPath: string,
+	{ href, label }: NavLink,
+): React.ReactNode {
+	return currentPath === href ? (
+		<span key={href} className="text-gray-500">
+			{label}
+		</span>
+	) : (
+		<Link key={href} href={href}>
+			{label}
+		</Link>
 	);
 }
