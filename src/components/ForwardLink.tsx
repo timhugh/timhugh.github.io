bc5ef8a74e5d28566f396bd6896536560795965c
Tim Heuett <tim.heuett@gmail.com>
2025-03-17 22:06:47 -0700

    don't add back param from root

--- src/components/ForwardLink.tsx
+++ src/components/ForwardLink.tsx
@@ -4,12 +4,16 @@ import React, { AnchorHTMLAttributes } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-export default function ForwardLink(
-	props: AnchorHTMLAttributes<HTMLAnchorElement>,
-): JSX.Element {
+interface ForwardLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+	href: string;
+}
+
+export default function ForwardLink(props: ForwardLinkProps): JSX.Element {
 	const currentPath = usePathname();
 
-	const href = `${props.href}?back=${currentPath}`;
+	const href =
+		currentPath == "/" ? props.href : `${props.href}?back=${currentPath}`;
+
 	return (
 		<Link {...props} href={href}>
 			{props.children}
