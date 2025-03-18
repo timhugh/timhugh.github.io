85316ff67160568dcde199f67ea1b5f88cb6a5ca
Tim Heuett <tim.heuett@gmail.com>
2025-03-17 22:06:36 -0700

    wrap back link in suspense (for useSearchParams)

--- src/components/BackLink.tsx
+++ src/components/BackLink.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import React, { AnchorHTMLAttributes } from "react";
+import React, { AnchorHTMLAttributes, Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 export default function BackLink(
 	props: AnchorHTMLAttributes<HTMLAnchorElement>,
 ): JSX.Element {
+	return (
+		<Suspense>
+			<LinkComponent {...props}>{props.children}</LinkComponent>
+		</Suspense>
+	);
+}
+
+function LinkComponent(props: AnchorHTMLAttributes<HTMLAnchorElement>) {
 	const searchParams = useSearchParams();
 	const backPath = searchParams.get("back");
 
