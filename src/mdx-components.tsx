e1e27785be4b68819714a3362f73ccbd574a3855
Tim Heuett <tim.heuett@gmail.com>
2025-09-23 21:59:20 -0700

    finish checkbox styling

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -6,10 +6,10 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
 		input: (props) => {
 			if (props.type === "checkbox") {
 				return (
-					<label className="">
-						<input type="checkbox" disabled={true} style={{ display: "none" }} />
-						<span style={{ display: "inline-block", width: "1.5em", height: "1.5em", border: "2px solid currentColor", borderRadius: "0.25em", marginRight: "0.25em", verticalAlign: "middle", textAlign: "center", lineHeight: "1.25em" }}>
-							<svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth="1">
+					<label className="checkbox">
+						<input type="checkbox" {...props} />
+						<span>
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth="2">
 								<path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
 							</svg>
 						</span>
