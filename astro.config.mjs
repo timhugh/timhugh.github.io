372593dd02011a3eb01e2461f92dbba3779b4383
Tim Heuett <tim.heuett@gmail.com>
2026-07-17 18:54:12 -0700

    bump astro 7+ and use mdx in place of remark gfm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
 import { defineConfig } from "astro/config";
-import { unified } from "@astrojs/markdown-remark";
 import tailwindcss from "@tailwindcss/vite";
-import remarkGfm from "remark-gfm";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-	site: "https://timheuett.com",
-	markdown: {
-		processor: unified({
-			remarkPlugins: [remarkGfm],
-		}),
-		shikiConfig: {
-			themes: {
-				light: "github-light",
-				dark: "github-dark",
-			},
-		},
-	},
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  site: "https://timheuett.com",
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
